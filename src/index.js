87e41fc9edfe480c74bfa47970b44cd77a835e7c
brunorosselli <bruno.rosselli@hotmail.com>
2022-02-11 10:58:11 -0300

    Remoção do WebVitals

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import './i18n';
 
 ReactDOM.render(
@@ -11,5 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-reportWebVitals();
